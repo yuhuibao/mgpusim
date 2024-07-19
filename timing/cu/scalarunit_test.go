6ff22d5859548f391559a95a50fdd7bf72d3abbe
gaotianh <gao.tianh99@gmail.com>
2024-07-19 07:05:58 -0400

    scalarunit test implement

--- timing/cu/scalarunit_test.go
+++ timing/cu/scalarunit_test.go
@@ -4,30 +4,13 @@ import (
 	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/sarchlab/akita/v3/mem/mem"
+	"github.com/sarchlab/akita/v3/sim"
 	"github.com/sarchlab/mgpusim/v3/emu"
 	"github.com/sarchlab/mgpusim/v3/insts"
 	"github.com/sarchlab/mgpusim/v3/kernels"
 	"github.com/sarchlab/mgpusim/v3/timing/wavefront"
 )
-
-// type mockScratchpadPreparer struct {
-// 	wfPrepared  *wavefront.Wavefront
-// 	wfCommitted *wavefront.Wavefront
-// }
-
-// func (sp *mockScratchpadPreparer) Prepare(
-// 	instEmuState emu.InstEmuState,
-// 	wf *wavefront.Wavefront,
-// ) {
-// 	sp.wfPrepared = wf
-// }
-
-// func (sp *mockScratchpadPreparer) Commit(
-// 	instEmuState emu.InstEmuState,
-// 	wf *wavefront.Wavefront,
-// ) {
-// 	sp.wfCommitted = wf
-// }
 
 var _ = Describe("Scalar Unit", func() {
 
@@ -70,7 +53,6 @@ var _ = Describe("Scalar Unit", func() {
 	})
 
 	It("should accept wave", func() {
-		// wave := new(wavefront.Wavefront)
 		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
 		bu.AcceptWave(wave, 10)
 		Expect(bu.toRead).To(BeIdenticalTo(wave))
@@ -98,14 +80,9 @@ var _ = Describe("Scalar Unit", func() {
 		Expect(bu.toWrite).To(BeIdenticalTo(wave2))
 		Expect(bu.toExec).To(BeIdenticalTo(wave1))
 		Expect(bu.toRead).To(BeNil())
-
-		// Expect(sp.wfPrepared).To(BeIdenticalTo(wave1))
-		// Expect(alu.wfExecuted).To(BeIdenticalTo(wave2))
-		// Expect(sp.wfCommitted).To(BeIdenticalTo(wave3))
 	})
 
 	It("should run s_load_dword", func() {
-		// wave := wavefront.NewWavefront(nil)
 		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
 		bu.toExec = wave
 
@@ -115,11 +92,8 @@ var _ = Describe("Scalar Unit", func() {
 		inst.Data = insts.NewSRegOperand(0, 0, 1)
 		wave.SetDynamicInst(inst)
 
-		// alu.EXPECT().ReadOperand(wave,).Return(0x1000)
-
-		// sp := wave.Scratchpad().AsSMEM()
-		// sp.Base = 0x1000
-		// sp.Offset = 0x24
+		alu.EXPECT().ReadOperand(wave, inst.Offset, 0, nil).Return(uint64(0x1000))
+		alu.EXPECT().ReadOperand(wave, inst.Base, 0, nil).Return(uint64(0x24))
 
 		//expectedReq := mem.NewReadReq(10, cu, scalarMem, 0x1024, 4)
 		//conn.ExpectSend(expectedReq, nil)
@@ -127,130 +101,135 @@ var _ = Describe("Scalar Unit", func() {
 		bu.Run(10)
 
 		Expect(wave.State).To(Equal(wavefront.WfReady))
-		// Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
-		// Expect(len(cu.InFlightScalarMemAccess)).To(Equal(1))
-		// //Expect(conn.AllExpectedSent()).To(BeTrue())
-		// Expect(bu.readBuf).To(HaveLen(1))
+		Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
+		Expect(len(cu.InFlightScalarMemAccess)).To(Equal(1))
+		//Expect(conn.AllExpectedSent()).To(BeTrue())
+		Expect(bu.readBuf).To(HaveLen(1))
 	})
 
-	// 	It("should run s_load_dwordx2", func() {
-	// 		wave := wavefront.NewWavefront(nil)
-	// 		bu.toExec = wave
+	It("should run s_load_dwordx2", func() {
+		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
 
-	// 		inst := wavefront.NewInst(insts.NewInst())
-	// 		inst.FormatType = insts.SMEM
-	// 		inst.Opcode = 1
-	// 		inst.Data = insts.NewSRegOperand(0, 0, 1)
-	// 		wave.SetDynamicInst(inst)
+		bu.toExec = wave
 
-	// 		sp := wave.Scratchpad().AsSMEM()
-	// 		sp.Base = 0x1000
-	// 		sp.Offset = 0x24
+		inst := wavefront.NewInst(insts.NewInst())
+		inst.FormatType = insts.SMEM
+		inst.Opcode = 1
+		inst.Data = insts.NewSRegOperand(0, 0, 1)
+		wave.SetDynamicInst(inst)
 
-	// 		//expectedReq := mem.NewReadReq(10, cu, scalarMem, 0x1024, 8)
-	// 		//conn.ExpectSend(expectedReq, nil)
+		alu.EXPECT().ReadOperand(wave, inst.Offset, 0, nil).Return(uint64(0x1000))
+		alu.EXPECT().ReadOperand(wave, inst.Base, 0, nil).Return(uint64(0x24))
 
-	// 		bu.Run(10)
+		//expectedReq := mem.NewReadReq(10, cu, scalarMem, 0x1024, 8)
+		//conn.ExpectSend(expectedReq, nil)
 
-	// 		Expect(wave.State).To(Equal(wavefront.WfReady))
-	// 		Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
-	// 		//Expect(len(cu.inFlightMemAccess)).To(Equal(1))
-	// 		//Expect(conn.AllExpectedSent()).To(BeTrue())
-	// 		Expect(bu.readBuf).To(HaveLen(1))
-	// 	})
+		bu.Run(10)
 
-	// 	It("should run s_load_dwordx4, access cross cacheline", func() {
-	// 		wave := wavefront.NewWavefront(nil)
-	// 		bu.toExec = wave
+		Expect(wave.State).To(Equal(wavefront.WfReady))
+		Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
+		// Expect(len(cu.inFlightMemAccess)).To(Equal(1))
+		//Expect(conn.AllExpectedSent()).To(BeTrue())
+		Expect(bu.readBuf).To(HaveLen(1))
+	})
 
-	// 		inst := wavefront.NewInst(insts.NewInst())
-	// 		inst.FormatType = insts.SMEM
-	// 		inst.Opcode = 2
-	// 		inst.Data = insts.NewSRegOperand(0, 0, 1)
-	// 		wave.SetDynamicInst(inst)
+	It("should run s_load_dwordx4, access cross cacheline", func() {
+		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		bu.toExec = wave
 
-	// 		sp := wave.Scratchpad().AsSMEM()
-	// 		sp.Base = 0x1000
-	// 		sp.Offset = 56
-	// 		start := sp.Base + sp.Offset
-	// 		bu.Run(10)
-	// 		Expect(bu.numCacheline(start, uint64(16))).To(Equal(2))
-	// 		Expect(wave.State).To(Equal(wavefront.WfReady))
-	// 		Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
-	// 		Expect(bu.readBuf).To(HaveLen(2))
-	// 		Expect(bu.readBuf[0].CanWaitForCoalesce).To(BeTrue())
-	// 		Expect(bu.readBuf[0].Address).To(Equal(uint64(0x1038)))
-	// 		Expect(bu.readBuf[0].AccessByteSize).To(Equal(uint64(8)))
-	// 		Expect(bu.readBuf[1].CanWaitForCoalesce).To(BeFalse())
-	// 		Expect(bu.readBuf[1].Address).To(Equal(uint64(0x1040)))
-	// 		Expect(bu.readBuf[1].AccessByteSize).To(Equal(uint64(8)))
-	// 	})
+		inst := wavefront.NewInst(insts.NewInst())
+		inst.FormatType = insts.SMEM
+		inst.Opcode = 2
+		inst.Data = insts.NewSRegOperand(0, 0, 1)
+		wave.SetDynamicInst(inst)
 
-	// 	It("should send request out", func() {
-	// 		req := mem.ReadReqBuilder{}.
-	// 			WithSendTime(10).
-	// 			WithSrc(cu.ToScalarMem).
-	// 			WithDst(scalarMem).
-	// 			WithAddress(1024).
-	// 			WithByteSize(4).
-	// 			Build()
-	// 		bu.readBuf = append(bu.readBuf, req)
+		base := uint64(0x1000)
+		offset := uint64(56)
 
-	// 		toScalarMem.EXPECT().Send(gomock.Any()).Do(func(r sim.Msg) {
-	// 			req := r.(*mem.ReadReq)
-	// 			Expect(req.Src).To(BeIdenticalTo(cu.ToScalarMem))
-	// 			Expect(req.Dst).To(BeIdenticalTo(scalarMem))
-	// 			Expect(req.Address).To(Equal(uint64(1024)))
-	// 			Expect(req.AccessByteSize).To(Equal(uint64(4)))
-	// 		})
+		alu.EXPECT().ReadOperand(wave, inst.Offset, 0, nil).Return(base)
+		alu.EXPECT().ReadOperand(wave, inst.Base, 0, nil).Return(offset)
 
-	// 		bu.Run(11)
+		start := base + offset
 
-	// 		Expect(bu.readBuf).To(HaveLen(0))
-	// 	})
+		bu.Run(10)
+		Expect(bu.numCacheline(start, uint64(16))).To(Equal(2))
+		Expect(wave.State).To(Equal(wavefront.WfReady))
+		Expect(wave.OutstandingScalarMemAccess).To(Equal(1))
+		Expect(bu.readBuf).To(HaveLen(2))
+		Expect(bu.readBuf[0].CanWaitForCoalesce).To(BeTrue())
+		Expect(bu.readBuf[0].Address).To(Equal(uint64(0x1038)))
+		Expect(bu.readBuf[0].AccessByteSize).To(Equal(uint64(8)))
+		Expect(bu.readBuf[1].CanWaitForCoalesce).To(BeFalse())
+		Expect(bu.readBuf[1].Address).To(Equal(uint64(0x1040)))
+		Expect(bu.readBuf[1].AccessByteSize).To(Equal(uint64(8)))
+	})
 
-	// 	It("should retry if send request failed", func() {
-	// 		req := mem.ReadReqBuilder{}.
-	// 			WithSendTime(10).
-	// 			WithSrc(cu.ToScalarMem).
-	// 			WithDst(scalarMem).
-	// 			WithAddress(1024).
-	// 			WithByteSize(4).
-	// 			Build()
-	// 		bu.readBuf = append(bu.readBuf, req)
+	It("should send request out", func() {
+		req := mem.ReadReqBuilder{}.
+			WithSendTime(10).
+			WithSrc(cu.ToScalarMem).
+			WithDst(scalarMem).
+			WithAddress(1024).
+			WithByteSize(4).
+			Build()
+		bu.readBuf = append(bu.readBuf, req)
 
-	// 		toScalarMem.EXPECT().Send(gomock.Any()).Do(func(r sim.Msg) {
-	// 			req := r.(*mem.ReadReq)
-	// 			Expect(req.Src).To(BeIdenticalTo(cu.ToScalarMem))
-	// 			Expect(req.Dst).To(BeIdenticalTo(scalarMem))
-	// 			Expect(req.Address).To(Equal(uint64(1024)))
-	// 			Expect(req.AccessByteSize).To(Equal(uint64(4)))
-	// 		}).Return(&sim.SendError{})
+		toScalarMem.EXPECT().Send(gomock.Any()).Do(func(r sim.Msg) {
+			req := r.(*mem.ReadReq)
+			Expect(req.Src).To(BeIdenticalTo(cu.ToScalarMem))
+			Expect(req.Dst).To(BeIdenticalTo(scalarMem))
+			Expect(req.Address).To(Equal(uint64(1024)))
+			Expect(req.AccessByteSize).To(Equal(uint64(4)))
+		})
 
-	// 		bu.Run(11)
+		bu.Run(11)
 
-	// 		Expect(bu.readBuf).To(HaveLen(1))
-	// 	})
-	// 	It("should flush the scalar unit", func() {
-	// 		wave := wavefront.NewWavefront(nil)
-	// 		inst := wavefront.NewInst(insts.NewInst())
-	// 		inst.FormatType = insts.SMEM
-	// 		inst.Opcode = 1
-	// 		inst.Data = insts.NewSRegOperand(0, 0, 1)
-	// 		wave.SetDynamicInst(inst)
+		Expect(bu.readBuf).To(HaveLen(0))
+	})
 
-	// 		bu.toExec = wave
-	// 		bu.toWrite = wave
-	// 		bu.toRead = wave
+	It("should retry if send request failed", func() {
+		req := mem.ReadReqBuilder{}.
+			WithSendTime(10).
+			WithSrc(cu.ToScalarMem).
+			WithDst(scalarMem).
+			WithAddress(1024).
+			WithByteSize(4).
+			Build()
+		bu.readBuf = append(bu.readBuf, req)
 
-	// 		bu.Flush()
+		toScalarMem.EXPECT().Send(gomock.Any()).Do(func(r sim.Msg) {
+			req := r.(*mem.ReadReq)
+			Expect(req.Src).To(BeIdenticalTo(cu.ToScalarMem))
+			Expect(req.Dst).To(BeIdenticalTo(scalarMem))
+			Expect(req.Address).To(Equal(uint64(1024)))
+			Expect(req.AccessByteSize).To(Equal(uint64(4)))
+		}).Return(&sim.SendError{})
 
-	// 		Expect(bu.toRead).To(BeNil())
-	// 		Expect(bu.toWrite).To(BeNil())
-	// 		Expect(bu.toExec).To(BeNil())
-	// 	})
+		bu.Run(11)
 
-	//	It("should return correct num of cacheline", func() {
-	//		Expect(bu.numCacheline(0x1038, uint64(80))).To(Equal(3))
-	//	})
+		Expect(bu.readBuf).To(HaveLen(1))
+	})
+
+	It("should flush the scalar unit", func() {
+		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		inst := wavefront.NewInst(insts.NewInst())
+		inst.FormatType = insts.SMEM
+		inst.Opcode = 1
+		inst.Data = insts.NewSRegOperand(0, 0, 1)
+		wave.SetDynamicInst(inst)
+
+		bu.toExec = wave
+		bu.toWrite = wave
+		bu.toRead = wave
+
+		bu.Flush()
+
+		Expect(bu.toRead).To(BeNil())
+		Expect(bu.toWrite).To(BeNil())
+		Expect(bu.toExec).To(BeNil())
+	})
+
+	It("should return correct num of cacheline", func() {
+		Expect(bu.numCacheline(0x1038, uint64(80))).To(Equal(3))
+	})
 })
