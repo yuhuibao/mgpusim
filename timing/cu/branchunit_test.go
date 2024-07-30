901eb34574b764dba30e0c4c282ecda181db8d94
gaotianh <gao.tianh99@gmail.com>
2024-07-30 12:10:08 -0400

    branchunit test

--- timing/cu/branchunit_test.go
+++ timing/cu/branchunit_test.go
@@ -1,94 +1,101 @@
 package cu
 
-// . "github.com/onsi/ginkgo/v2"
-// . "github.com/onsi/gomega"
+import (
+	gomock "github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/sarchlab/mgpusim/v3/emu"
+	"github.com/sarchlab/mgpusim/v3/insts"
+	"github.com/sarchlab/mgpusim/v3/kernels"
+	"github.com/sarchlab/mgpusim/v3/timing/wavefront"
+)
 
-// var _ = Describe("Branch Unit", func() {
+var _ = Describe("Branch Unit", func() {
 
-// 	var (
-// 		cu  *ComputeUnit
-// 		bu  *BranchUnit
-// 		alu *mockALU
-// 	)
+	var (
+		mockCtrl *gomock.Controller
+		cu       *ComputeUnit
+		bu       *BranchUnit
+		alu      *MockALU
+	)
 
-// 	BeforeEach(func() {
-// 		cu = NewComputeUnit("CU", nil)
-// 		alu = new(mockALU)
-// 		bu = NewBranchUnit(cu, alu)
-// 	})
+	BeforeEach(func() {
+		mockCtrl = gomock.NewController(GinkgoT())
+		cu = NewComputeUnit("CU", nil)
+		alu = NewMockALU(mockCtrl)
+		bu = NewBranchUnit(cu, alu)
+	})
 
-// 	It("should allow accepting wavefront", func() {
-// 		// wave := new(wavefront.Wavefront)
-// 		bu.toRead = nil
-// 		Expect(bu.CanAcceptWave()).To(BeTrue())
-// 	})
+	It("should allow accepting wavefront", func() {
+		bu.toRead = nil
+		Expect(bu.CanAcceptWave()).To(BeTrue())
+	})
 
-// 	It("should not allow accepting wavefront is the read stage buffer is occupied", func() {
-// 		bu.toRead = new(wavefront.Wavefront)
-// 		Expect(bu.CanAcceptWave()).To(BeFalse())
-// 	})
+	It("should not allow accepting wavefront is the read stage buffer is occupied", func() {
+		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		bu.toRead = wave
+		Expect(bu.CanAcceptWave()).To(BeFalse())
+	})
 
-// 	It("should accept wave", func() {
-// 		wave := new(wavefront.Wavefront)
-// 		bu.AcceptWave(wave, 10)
-// 		Expect(bu.toRead).To(BeIdenticalTo(wave))
-// 	})
+	It("should accept wave", func() {
+		wave := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		bu.AcceptWave(wave, 10)
+		Expect(bu.toRead).To(BeIdenticalTo(wave))
+	})
 
-// 	It("should run", func() {
-// 		wave1 := new(wavefront.Wavefront)
-// 		wave2 := new(wavefront.Wavefront)
-// 		wave3 := new(wavefront.Wavefront)
-// 		wave3.State = wavefront.WfRunning
-// 		wave3.InstBuffer = make([]byte, 256)
-// 		wave3.InstBufferStartPC = 0x100
-// 		inst := wavefront.NewInst(insts.NewInst())
-// 		inst.FormatType = insts.SOPP
-// 		inst.SImm16 = insts.NewIntOperand(1, 1)
-// 		inst.ByteSize = 4
-// 		wave3.SetDynamicInst(inst)
-// 		wave3.PC = 0x13C
+	It("should run", func() {
+		wave1 := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		wave2 := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		wave3 := wavefront.NewWavefront(emu.NewWavefront(new(kernels.Wavefront)))
+		wave3.State = wavefront.WfRunning
+		wave3.InstBuffer = make([]byte, 256)
+		wave3.InstBufferStartPC = 0x100
+		inst := wavefront.NewInst(insts.NewInst())
+		inst.FormatType = insts.SOPP
+		inst.SImm16 = insts.NewIntOperand(1, 1)
+		inst.ByteSize = 4
+		wave3.SetDynamicInst(inst)
+		wave3.PC = 0x13C
 
-// 		bu.toRead = wave1
-// 		bu.toExec = wave2
-// 		bu.toWrite = wave3
+		bu.toRead = wave1
+		bu.toExec = wave2
+		bu.toWrite = wave3
 
-// 		bu.Run(10)
+		alu.EXPECT().Run(wave2).Times(1)
 
-// 		Expect(wave3.State).To(Equal(wavefront.WfReady))
-// 		Expect(wave3.PC).To(Equal(uint64(0x140)))
+		bu.Run(10)
 
-// 		Expect(bu.toWrite).To(BeIdenticalTo(wave2))
-// 		Expect(bu.toExec).To(BeIdenticalTo(wave1))
-// 		Expect(bu.toRead).To(BeNil())
+		Expect(wave3.State).To(Equal(wavefront.WfReady))
+		Expect(wave3.PC).To(Equal(uint64(0x140)))
 
-// 		Expect(sp.wfPrepared).To(BeIdenticalTo(wave1))
-// 		Expect(alu.wfExecuted).To(BeIdenticalTo(wave2))
-// 		Expect(sp.wfCommitted).To(BeIdenticalTo(wave3))
-// 		Expect(wave3.InstBuffer).To(HaveLen(0))
-// 	})
+		Expect(bu.toWrite).To(BeIdenticalTo(wave2))
+		Expect(bu.toExec).To(BeIdenticalTo(wave1))
+		Expect(bu.toRead).To(BeNil())
+		Expect(wave3.InstBuffer).To(HaveLen(0))
+	})
 
-// 	It("should flush", func() {
-// 		wave1 := new(wavefront.Wavefront)
-// 		wave2 := new(wavefront.Wavefront)
-// 		wave3 := new(wavefront.Wavefront)
-// 		wave3.State = wavefront.WfRunning
-// 		wave3.InstBuffer = make([]byte, 256)
-// 		wave3.InstBufferStartPC = 0x100
-// 		inst := wavefront.NewInst(insts.NewInst())
-// 		inst.FormatType = insts.SOPP
-// 		inst.SImm16 = insts.NewIntOperand(1, 1)
-// 		inst.ByteSize = 4
-// 		wave3.SetDynamicInst(inst)
+	It("should flush", func() {
+		wave1 := new(wavefront.Wavefront)
+		wave2 := new(wavefront.Wavefront)
+		wave3 := new(wavefront.Wavefront)
+		wave3.State = wavefront.WfRunning
+		wave3.InstBuffer = make([]byte, 256)
+		wave3.InstBufferStartPC = 0x100
+		inst := wavefront.NewInst(insts.NewInst())
+		inst.FormatType = insts.SOPP
+		inst.SImm16 = insts.NewIntOperand(1, 1)
+		inst.ByteSize = 4
+		wave3.SetDynamicInst(inst)
 
-// 		bu.toRead = wave1
-// 		bu.toExec = wave2
-// 		bu.toWrite = wave3
+		bu.toRead = wave1
+		bu.toExec = wave2
+		bu.toWrite = wave3
 
-// 		bu.Flush()
+		bu.Flush()
 
-// 		Expect(bu.toRead).To(BeNil())
-// 		Expect(bu.toWrite).To(BeNil())
-// 		Expect(bu.toExec).To(BeNil())
+		Expect(bu.toRead).To(BeNil())
+		Expect(bu.toWrite).To(BeNil())
+		Expect(bu.toExec).To(BeNil())
 
-// 	})
-// })
+	})
+})
